6489373e06c4b0e4a360853f1bcaefab2e2eeae8
feditheanalyst <114922995+feditheanalyst@users.noreply.github.com>
2024-01-01 12:43:48 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -2,7 +2,7 @@ import random
 import json
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import streamlit as st
 from streamlit_option_menu import option_menu
 import mysql.connector
